86d8ba56efa7b9f626e6c897b8f5b43b9533628a
Will Woodacre <woodacrew@gmail.com>
2025-02-16 14:22:49 +0000

    Initial script

--- scripts/draw.py
+++ scripts/draw.py
@@ -1,3 +1,15 @@
 from inky.auto import auto
+from html2image import Html2Image
+import os
+from PIL import Image
+
+PATH = os.path.dirname(__file__)
+
+
+hti = Html2Image(size=(800, 480))
+hti.screenshot(url='http://localhost:3000', save_as='screenshot.png')
 
 display = auto()
+img = Image.open(os.path.join(PATH, 'screenshot.png'))
+display.set_image(img)
+display.show()
